3b943d9c7852c48c8c911aca062efad433172d1b
Carolina Veloso <annacarolinacampos@hotmail.com>
2024-03-12 21:11:19 -0300

    Aula 4

--- src/Aula4.java
+++ src/Aula4.java
@@ -36,4 +36,30 @@ public class Aula4 {
         media = (n1 + n2 + n3) / 3;
         System.out.println("A média das notas é: " + media);
     }
+    public static void ex7() {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+        int n1, n2, maior;
+        System.out.println("Digite o primeiro número: ");
+        n1 = Integer.parseInt(input.nextLine());
+        System.out.println("Digite o segundo número: ");
+        n2 = Integer.parseInt(input.nextLine());
+        maior = (n1>n2) ? n1 : n2;
+        System.out.println("O maior número é: " + maior);
+    }
+
+    public static void ex8(){
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+        int n1, n2, n3, aux, maior;
+        System.out.println("Digite o primeiro número: ");
+        n1 = Integer.parseInt(input.nextLine());
+        System.out.println("Digite o segundo número: ");
+        n2 = Integer.parseInt(input.nextLine());
+        System.out.println("Digite o terceiro número: ");
+        n3 = Integer.parseInt(input.nextLine());
+        aux = (n1>n2) ? n1 : n2;
+        maior = (aux>n3) ? aux:n3;
+        System.out.println(maior);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Aula4.ex2();
+        Aula4.ex8();
 
     }
 }
